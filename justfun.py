765fd12ca6ff6cd2acbcb5b61bfdcab6a0250161
notverynicex <xnicenonikx@gmail.com>
2020-05-11 01:34:26 +0300

    Commit #2. Added another module. Just an alt for print, I guess.

--- justfun.py
+++ justfun.py
@@ -1,5 +1,7 @@
 from module import listchanger
+from module import just_copied
 print("Insert 3 numbers")
+#TODO Make this look better
 a = int(input())
 x = int(input())
 y = int(input())
@@ -7,3 +9,8 @@ list = [a, x, y]
 print(f"The values before the module runs is {list}")
 listchanger(list)
 print(f"These are the values after the listchanger module {list}")
+
+print("Insert some text please")
+text = str(input())
+just_copied(text)
+
--- module.py
+++ module.py
@@ -1,3 +1,7 @@
 def listchanger(changer1):
     changer1[1] = 33
     return
+
+def just_copied(macheen):
+    print(macheen)
+
